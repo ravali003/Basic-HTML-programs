f22da2ce869a943723877f106665b8724ee419ae
ravali003 <34567004+ravali003@users.noreply.github.com>
2017-12-15 13:30:49 +0530

    Add files via upload

---
+++ abc.php
@@ -0,0 +1,11 @@
+<html>
+<form action="search.php" method="get" />
+<table align="center" valign="center">
+<tr>
+<td>a<input type="text" name="regdno" maxlength="12" />
+<input type="submit" name="submit" value="search.."/>
+</td>
+</tr>
+</table>
+</form>
+</html>
